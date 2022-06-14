9aa73b6e6965a129c7f3e0290325e734b678980a
matteotagliatti <matteotagliatti@gmail.com>
2022-06-14 12:32:00 +0200

    edit db seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(TypesTableSeeder::class);
         $this->call(UserTypeTableSeeder::class);
         $this->call(OrdersTableSeeder::class);
+        $this->call(DishOrderTableSeeder::class);
     }
 }
