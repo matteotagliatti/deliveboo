a71ca2fcc4f62cc12e59810429c7225d8d12f7f8
Anto231996 <tony2396@hotmail.it>
2022-06-21 12:44:55 +0200

    doing homepage

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -43,6 +43,72 @@
                 </div>
             </div>
         </div>
+        <!-- cta -->
+        <div class="row py-5 mb-5">
+            <div class="col-12">
+                <div id="cta" class="container py-5">
+                    <div class="overlay"></div>
+                    <div id="cta-text" class="text-white d-flex justify-content-center flex-column ml-4">
+                        <h3>Hai un ristorante?</h3>
+                        <a class="btn btn-warning w-50">Registrati</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ordina ora -->
+        <!-- <div class="row py-5">
+            <div class="col-12">
+                <div id="order-now" class="container py-5">
+                    <div class="row">
+                        <div class="col-6">
+                            <div></div>
+                        </div>
+
+                        <div class="col-6">
+
+                        </div>
+                </div>
+            </div>
+        </div> -->
+
+        <div id="order-now" class="container py-5">
+            <div class="row h-100 py-5">
+                <div class="col-7">
+                    <div class="row h-100">
+                        <div class="col-6 d-flex flex-column justify-content-between">
+                            <div class="card mb-3 p-2">
+                                <img src="/img/how_1.svg" class="card-img-top" alt="...">
+                                <div class="card-body text-center">
+                                    <h5 class="card-title font-weight-bold">Ordina facilmente</h5>
+                                    <p class="card-text">Ordina i tuoi piatti preferiti in modo semplice e veloce grazie alla nostra app.</p>
+                                </div>
+                            </div>
+
+                            <div class="card p-2">
+                                <img src="/img/how_2.svg" class="card-img-top" alt="...">
+                                <div class="card-body text-center">
+                                    <h5 class="card-title font-weight-bold">Consegne veloci</h5>
+                                    <p class="card-text">Ricevi i tuoi piatti direttamente a casa tua in tutta sicurezza.</p>
+                                </div>
+                            </div>
+                    
+                        </div>
+                        <div class="col-6 d-flex flex-column justify-content-center">
+                            <div class="card p-2">
+                                <img src="/img/how_3.svg" class="card-img-top" alt="...">
+                                <div class="card-body text-center">
+                                    <h5 class="card-title font-weight-bold">Goditi il cibo</h5>
+                                    <p class="card-text">Condividi i piatti con chi vuoi e dove vuoi. Con amici e famiglia.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-5">
+                    IGRID
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
