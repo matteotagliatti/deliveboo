3060a2a53f4db7faa9207a2ffaba82cf4c13705f
matteotagliatti <matteotagliatti@gmail.com>
2022-06-21 09:58:52 +0200

    start frontend

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,9 +1,19 @@
 <template>
-    <div class="row mt-5">
-        <div class="col-12 d-flex justify-content-center">
-            <div>
-                <h1>Work in Progress</h1>
-                <h2>Sito in Costruzione</h2>
+    <div>
+        <!-- Hero -->
+        <div class="bg-dark row">
+            <div class="col-12 d-flex justify-content-center">
+                <div class="text-center my-py-13 text-white">
+                    <h1>Cerca i tuoi cibi preferiti</h1>
+                    <a class="btn btn-primary" href="#">Tutti i ristoranti</a>
+                </div>
+            </div>
+        </div>
+        <!-- Ristoranti -->
+        <div class="row">
+            <div class="col-12 container text-center">
+                <h2>I migliori ristoranti</h2>
+                <div class="my-grid"></div>
             </div>
         </div>
     </div>
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -12,21 +12,21 @@
 </head>
 
 <body>
-    <header class="position-fixed fixed-top">
+    <header class="position-fixed fixed-top text-white">
         <div class="container d-flex justify-content-between align-items-center py-3">
-            <h1 class="fw-bold">DeliveBoo</h1>
+            <a class="my-logo-text text-white font-weight-bold" href="/">DeliveBoo</a>
             <nav>
-                <ul class="d-flex gap-3 list-unstyled">
-                    <li>Cerca</li>
-                    <li>Ristoranti</li>
+                <ul class="d-flex gap-2 list-unstyled">
+                    <li><a class="text-white" href="#">Cerca</a></li>
+                    <li><a class="text-white" href="#">Ristoranti</a></li>
                     @if (Route::has('login'))
                         @auth
-                            <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
+                            <li><a class="text-white" href="{{ url('/dashboard') }}">Dashboard</a></li>
                         @else
-                            <li><a href="{{ route('login') }}">Login Ristorante</a></li>
+                            <li><a class="text-white" href="{{ route('login') }}">Login Ristorante</a></li>
 
                             @if (Route::has('register'))
-                                <li><a href="{{ route('register') }}">Registrati Ristorante</a></li>
+                                <li><a class="text-white" href="{{ route('register') }}">Registrati Ristorante</a></li>
                             @endif
                         @endauth
                     @endif
