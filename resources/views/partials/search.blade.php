80370796b9c5d2c62872946a47c666539c0b5d75
matteotagliatti <matteotagliatti@gmail.com>
2022-06-24 10:46:08 +0200

    rm div

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,18 +1,17 @@
-<div>
-    <div id="hero" class="position-relative bg-dark">
-        <div id="text-hero" class="text-center my-py-13 text-white position-absolute">
-            <h1>Cerca</h1>
-            <form action="{{ route('search.update') }}" method="post">
-                @csrf
+<div id="hero" class="position-relative bg-dark">
+    <div id="text-hero" class="text-center my-py-13 text-white position-absolute">
+        <h1>Cerca</h1>
+        <form action="{{ route('search.update') }}" method="post">
+            @csrf
 
-                <select class="form-control mb-2" name="types" id="types">
-                    <option>Scegli la tipologia</option>
-                    @foreach ($types as $type)
-                        <option value="{{ $type->id }}">{{ ucFirst($type->nome) }}</option>
-                    @endforeach
-                </select>
-                <input class="btn btn-primary" type="submit" value="Cerca">
-            </form>
+            <select class="form-control mb-2" name="types" id="types">
+                <option>Scegli la tipologia</option>
+                @foreach ($types as $type)
+                    <option value="{{ $type->id }}">{{ ucFirst($type->nome) }}</option>
+                @endforeach
+            </select>
+            <input class="btn btn-primary" type="submit" value="Cerca">
+        </form>
 
-        </div>
     </div>
+</div>
