f14f55f6ab72f2b664476b3c39411a9c854d25bb
matteotagliatti <matteotagliatti@gmail.com>
2022-06-25 20:38:40 +0200

    add statistics, order date from 2020

--- database/seeds/OrdersTableSeeder.php
+++ database/seeds/OrdersTableSeeder.php
@@ -15,13 +15,14 @@ class OrdersTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         $user_ids = User::pluck('id')->toArray();
-
-        for ($i = 0; $i < 20; $i++) {
+        
+        for ($i = 0; $i < 60; $i++) {
+            $dt = $faker->dateTimeBetween($startDate = '-2 years', $endDate = 'now');
             $newOrder = new Order();
             $newOrder->user_id = $faker->randomElement($user_ids);
             $newOrder->prezzo = $faker->randomFloat(2, 10, 200);
             $newOrder->indirizzo = $faker->address();
-            $newOrder->data_e_ora = $faker->date('Y-m-d') . " " . $faker->time('H:i:s');
+            $newOrder->data_e_ora = $dt->format('Y-m-d') . " " . $faker->time('H:i:s');
             $newOrder->nome = $faker->firstName();
             $newOrder->cognome = $faker->lastName();
             $newOrder->telefono = $faker->phoneNumber();
--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -11,7 +11,7 @@
         </div>
         <div class="row">
             <div class="col-12">
-                <table class="table">
+                <table class="table mb-5">
                     <thead>
                         <tr>
                             <th scope="col">ID Ordine</th>
@@ -36,7 +36,7 @@
                                 </td>
                                 <td scope="row">{{ $order->prezzo }}</td>
                                 <td scope="row">{{ $order->indirizzo }}</td>
-                                <td scope="row">{{ $order->data_e_ora }}</td>
+                                <td scope="row" class="datetime">{{ $order->data_e_ora }}</td>
                                 <td scope="row">{{ $order->nome }}</td>
                                 <td scope="row">{{ $order->cognome }}</td>
                                 <td scope="row">{{ $order->telefono }}</td>
@@ -52,5 +52,112 @@
             </div>
 
         </div>
+        <div class="row">
+            <div class="col-12">
+                <h1>Statistiche</h1>
+                <div class="row">
+                    <div class="col-6">
+                        <p>Ordini per mese</p>
+                        <div>
+                            <canvas id="myChart"></canvas>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <p>Ordini per anno</p>
+                        <div>
+                            <canvas id="myChart2"></canvas>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
+@endsection
+
+@section('script')
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        const datetimes = document.querySelectorAll('.datetime');
+        const datetimesValues = Array.from(datetimes).map(datetime => datetime.textContent);
+        const dates = datetimesValues.map(datetime => datetime.split(' ')[0]);
+        const years = dates.map(date => date.split('-')[0]);
+        const months = dates.map(date => date.split('-')[1]);
+        // set months with no iteration to 0 count every istance of each month in months array
+        const monthsCount = months.reduce((acc, curr) => {
+            acc[curr] = acc[curr] ? acc[curr] + 1 : 1;
+            return acc;
+        }, {});
+        const yearsCount = years.reduce((acc, curr) => {
+            acc[curr] = acc[curr] ? acc[curr] + 1 : 1;
+            return acc;
+        }, {});
+
+        const labels = [
+            'Gennaio',
+            'Febbraio',
+            'Marzo',
+            'Aprile',
+            'Maggio',
+            'Giugno',
+            'Luglio',
+            'Agosto',
+            'Settembre',
+            'Ottobre',
+            'Novembre',
+            'Dicembre'
+        ];
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'Ordini ricevuti',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [monthsCount['01'], monthsCount['02'], monthsCount['03'], monthsCount['04'], monthsCount[
+                        '05'], monthsCount['06'], monthsCount['07'], monthsCount['08'], monthsCount['09'],
+                    monthsCount['10'], monthsCount['11'], monthsCount['12']
+                ]
+            }]
+        };
+
+        const config = {
+            type: 'bar',
+            data: data,
+            options: {}
+        };
+
+        const myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+        );
+
+        /* Seconda chart */
+        const labels2 = [
+            '2020',
+            '2021',
+            '2022',
+        ];
+
+        const data2 = {
+            labels: labels2,
+            datasets: [{
+                label: 'Ordini ricevuti dopo il 2020',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [yearsCount['2020'], yearsCount['2021'], yearsCount['2022']]
+            }]
+        };
+
+        const config2 = {
+            type: 'bar',
+            data: data2,
+            options: {}
+        };
+
+        const myChart2 = new Chart(
+            document.getElementById('myChart2'),
+            config2
+        );
+    </script>
 @endsection
