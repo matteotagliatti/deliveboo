c7fcfbca323919ac6dd57b2b01330d9242b6c676
matteotagliatti <matteotagliatti@gmail.com>
2022-06-26 16:14:08 +0200

    list none mail

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -6,13 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Email</title>
+    <style>
+        ul li {
+            list-style: none;
+        }
+    </style>
 </head>
 
 <body>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                @yield('content')
+                <div class="container">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <p>DeliveBoo - Grazie per averci scelto!</p>
             </div>
         </div>
     </div>
